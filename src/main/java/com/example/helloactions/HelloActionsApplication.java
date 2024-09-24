f77a98756e83a8b7ce156eab60afc8e8f39cc683
jazzyzoney <jaqtash22@gmail.com>
2024-09-24 10:50:37 +0200

    local

---
+++ src/main/java/com/example/helloactions/HelloActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloActionsApplication.class, args);
+    }
+
+}
